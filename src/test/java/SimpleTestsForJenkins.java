413210bcd55edc0f5d2c4191f5a1fefbe9e3b025
vladimir klonin <vklonin@gmail.com>
2021-02-17 22:07:31 +0400

    second test added

--- src/test/java/SimpleTestsForJenkins.java
+++ src/test/java/SimpleTestsForJenkins.java
@@ -1,9 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleTestsForJenkins {
 
-void simplePositiveTest(){
-    assertEquals(true, true);
-}
+    @Test
+    void simplePositiveTest(){
+        assertEquals(true, true);
+    }
+
+    @Test
+    void simpleNegativeTest(){
+        assertEquals(true, false);
+    }
 
 }
